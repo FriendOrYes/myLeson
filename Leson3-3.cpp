4202b75b5ef2bab346d9dba5f281ef9f184b78ca
Anton Chaban <anton.chaban@mail.ru>
2016-11-15 21:38:47 +0200

    Задание 16

---
+++ Leson3-3.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int arr[3][3] = { 0 };
+    int arr1[3][3] = { 0 };
+    int arr2[3][3] = { 0 };
+
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j <3; j++) {
+            cout << "Matrix 1st [" << i << "][" << j << "]:\n";
+            cin >> arr[i][j];
+        }
+    }
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cout << "Matrix 2nd [" << i << "][" << j << "]:\n";
+            cin >> arr1[i][j];
+        }
+    }
+
+    //write your logic here
+
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            for (int n = 0; n < 3; n++) {
+                arr2[i][j] += arr[i][n] * arr1[n][j];
+            }
+        }
+    }
+
+
+
+
+    //end
+
+
+    cout << "Multiply of both matrix:\n";
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cout << arr2[i][j] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
