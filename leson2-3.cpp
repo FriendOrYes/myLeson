1c0fbb968fd62db627ea3bffc8f2d9acedb5a1ad
Anton Chaban <anton.chaban@mail.ru>
2016-11-15 19:56:51 +0200

    Задание 9

--- leson2-3.cpp
+++ leson2-3.cpp
@@ -4,16 +4,21 @@
 using namespace std;
 void coding_string(const char* s, char *s2 ,const char *call_number){
     int size_shifr = strlen(call_number);
-    for(int i = 0;s[i] != 0 ; i++)
+    for(int i = 0;s[i]; i++)
     {
         s2[i] = s[i];
-        for(int j = 0 ; call_number[j] != 0 ; j++){
-            if(s[i] == call_number[j]){
-                if(j+5 > size_shifr){
-                    j = j+5 - size_shifr;
-                }
-                j+5 > size_shifr?j = j+5 - size_shifr:j+=5;
-                s2[i] = call_number[j];
+        cout<< s2[i]<<endl;
+        for(int j = 0 ; call_number[j] ; j++){
+            int k = j;
+            cout <<"call_number[ "<< k <<"] = "<< call_number[k] << endl;
+            if(s[i] == call_number[k]){//если нашли подходящий елемент из шифр таблицы
+               //проверяем выходит ли он за придел масива при кодировке
+               //cout <<"j + 5 ="<< j + 5 << endl ;
+               //если да то делаем кольцо и начинаем с начала
+               //если нет то переходим на 5 элементов вперёд и шифруем
+                k = j;
+                j+5 > size_shifr? k = j+5 - size_shifr:k+=5;//
+                s2[i] = call_number[k];
             }
         }
     }
@@ -29,7 +34,7 @@ int main(int argc, char *argv[])
     gets_s(s,size);
     coding_string(s,s2,call_number);
     printf("Original string %s\n",s);
-    printf("Coded string%s\n",s2);
+    printf("Coded %s\n",s2);
     delete[]s;
     delete[]s2;
     return 0;
