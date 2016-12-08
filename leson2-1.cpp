7555b11c841557a08987b781ee5c451904708a9b
Anton Chaban <anton.chaban@mail.ru>
2016-12-08 22:48:24 +0200

    Task 7 change

--- leson2-1.cpp
+++ leson2-1.cpp
@@ -1,15 +1,11 @@
-#include <iostream>
+﻿#include <iostream>
 #include <stdio.h>
 using namespace std;
 
-char* CS_init(char *s,int size){
-    s = new char[size];
-    memset(s,0,(size)*sizeof(char));//обнуляем строку
-    return s;
-}
+
 char* cat(char *s1 ,const char *s2){
     char *s3 = s1;//создаём указатель s3
-    while(*s3)s3++;//пробегаем  строку s1 и ищем конец её
+    s3 += strlen(s1);//передвинуть указатель на длину 1 строки что бы попасть на конец её	
     while(*s3++ = *s2++);//берём с начала строки 2 и начинаем добовлять к строке 1 пока не найдём конец строки 1
     return s1;//возвращаем указатель на s1
 }
@@ -18,10 +14,8 @@ int main(int argc, char *argv[])
     /*Инициализация строк */
     const int size = 100;
     char* s1 , *s2;
-    s1 = new char[size];
-    memset(s1,0,(size)*sizeof(char));//обнуляем строку
-    s2 = new char[size];
-    memset(s2,0,(size)*sizeof(char));//обнуляем строку
+    s1 = new char[size]();
+    s2 = new char[size]();
     /*запонение строк */
     gets_s(s1,size);
     gets_s(s2,size);
