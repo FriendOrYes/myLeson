d0cc880567be05c83d70ac58ddca3a7bed355e0f
FriendOrYes <anton.chaban@mail.ru>
2016-12-09 01:44:05 +0200

    Task 11 - change

--- leson2-5.cpp
+++ leson2-5.cpp
@@ -1,29 +1,38 @@
 #include <iostream>
 
 using namespace std;
-void char_del(char *s){
-       for(int i = 0 ; s[i] != 0 ; i++){
-           if( i == 0 && isalpha(s[i])){
-               for(int j = i ;s[j] != 0 ; j++ )
-                   s[j] = s[j+1];
-           }
-           if(s[i] == ' '){
-               for(int j = i+1 ;s[j] != 0 ; j++ )
-               {
-                   s[j] = s[j+1];
-                   cout << j << "-" << s[j] << endl;
-               }
-           }
+
+void char_del(char*& s){
+  int j = 0;
+  char *s2 = new char[strlen(s)];
+  for(int i = 0 ; s[i] != 0 ;i++){
+       if( i == 0 && isalpha(s[i])){
+           cout << s[i] << " - " << i <<endl;
+           continue;
        }
+       if(s[i] == ' '){
+           cout << s[i]<< " - " << i  << endl;
+           s2[j] = s[i];
+           i++;
+           j++;
+       }else{
+           cout << s[i]<< " - " << i << endl;
+           s2[j] = s[i];
+           j++;
+       }
+
+    }
+  s2[j] = '\0';
+  delete []s;
+  s = s2;
 }
+
 int main(int argc, char *argv[])
 {
     int size = 100 ;
-    char *s ;
-    s = new char[size];
+    char *s = new char[size]();
     gets_s(s,size);
     char_del(s);
-    printf("Строка после преобразования");
     printf(s);
     delete []s;
     return 0;
