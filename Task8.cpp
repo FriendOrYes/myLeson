ebd353d03c6503a9f0fe70987061831f0b0b8671
Anton Chaban <anton.chaban@mail.ru>
2016-12-25 22:00:53 +0200

    Task 8 fixed

---
+++ Task8.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+void FooMap(int* const  symbols, char* const  s, const int size, const int sizeMap);
+
+//---------------------------------------------------------------
+int main()
+{
+    const int size = 5;
+    const int sizeMap = 256;
+
+    int symbols[sizeMap] = {};
+    char s[size] = "dsdq";
+
+    FooMap(symbols, s, size, sizeMap);
+
+    return 0;
+}
+//---------------------------------------------------------------
+
+void FooMap(int* const  symbols, char* const  s, const int size, const int sizeMap){
+
+    for(int i = 0; i < size; ++i)
+    {
+        symbols[s[i]]+=1;
+    }
+
+    for(int i = 0; i < sizeMap; ++i)
+    {
+        if(symbols[i] > 0)
+        {
+            char c = i;
+            cout << c << ": " << symbols[i] << endl;
+        }
+    }
+}
