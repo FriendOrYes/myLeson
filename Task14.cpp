c6747f385bd11bc64995faf3346b1ffb33cd94ca
Anton Chaban <anton.chaban@mail.ru>
2016-12-25 23:19:13 +0200

    Task 14 change

---
+++ Task14.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <random>
+#include <ctime>
+
+using namespace std;
+
+
+#define ARRAY_SIZE(a) (sizeof(a)/sizeof(a[0]))
+void CreatPassword(char *password)
+{
+    int limits[] = {3, 2, 2, 1};
+    int sum = 0, i = 0;
+
+    auto sumFunc = [&]()
+    {
+        for (int i = 0; i < ARRAY_SIZE(limits); ++i)
+        {
+            sum += limits[i];
+        }
+    };
+    sumFunc();
+
+    while(i < sum)
+    {
+        int j = rand()%4;
+        if(limits[j])
+        {
+            switch (j) {
+            case 0:
+                password[i] = rand()%10 + 48;
+                break;
+            case 1:
+                password[i] = rand()%26 + 97;
+                break;
+            case 2:
+                password[i] = rand()%26 + 65;
+                break;
+            default:
+                password[i] = '_';
+                break;
+            }
+            i++;
+            limits[j]--;
+        }
+    }
+    password[i - 1] = '\0';
+}
+
+int main(int argc, char *argv[])
+{
+    srand(time(NULL));
+    const int size = 8;
+    char password[size] = {};
+    CreatPassword(password);
+    printf(" Encrypted String - %s \n",password);
+    return 0;
+}
