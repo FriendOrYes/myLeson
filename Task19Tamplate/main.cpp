a3d152ea177551df058646a25e7f6175e880c9ba
Anton Chaban <anton.chaban@mail.ru>
2017-01-09 15:26:25 +0200

    Tamplate function 24 Task

---
+++ Task19Tamplate/main.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <random>
+#include <ctime>
+#include <cstring>
+
+#include "templatefunction.h"
+
+using namespace std;
+
+int Sum(int x, int y)
+{
+     return  x + y;
+}
+
+bool MoreThenTwo(int x)
+{//value more then two return 1
+    if(x > 2)
+    {
+        return 1;
+    }
+    return 0;
+}
+
+
+int AddForech(int x)
+{
+    return x + 1;
+}
+
+//---------------------------------------------------------------------------
+int main()
+{
+    srand(time(0));// рандома к времини ОС
+
+    const int size = 10;
+    int x[size];
+
+    InitMas(x, size);
+
+    PrintMas(x, size);
+    cout << endl;
+
+    Map(x, size, AddForech);
+
+    PrintMas(x, size);
+
+    cout << endl;
+
+    int newSize;
+
+    Filter(x, size, newSize, MoreThenTwo);
+
+    PrintMas(x,newSize);
+    cout << endl;
+
+    cout << Redus(x, newSize, Sum) << endl;
+    return 0;
+}
---
+++ Task19Tamplate/templatefunction.h
@@ -0,0 +1,85 @@
+#ifndef TEMPLATEFUNCTION_H
+#define TEMPLATEFUNCTION_H
+#include <random>
+
+
+////---------------------------------------------------------------------------
+
+template<typename T>
+void InitMas(T x[], const int& size)
+{
+    for(int i = 0; i < size; ++i)
+    {
+        x[i] = rand()%10;
+    }
+}
+template<>
+void InitMas<char>(char x[], const int& size)
+{
+    for(int i = 0; i < size; ++i)
+    {
+        x[i] = rand()%25 + 65;
+    }
+}
+
+template<>
+void InitMas<bool>(bool x[], const int& size)
+{
+    for(int i = 0; i < size; ++i)
+    {
+        x[i] = rand()%2;
+    }
+}
+template<typename T>
+void PrintMas(T x[], const int& size)
+{
+    for(int i = 0; i < size; ++i)
+    {
+        cout << x[i] << " ";
+    }
+}
+//---------------------------------------------
+template<typename T>
+T *Map(T x[], const int& size, T (*pMapFunc)(T))
+{
+    cout << __FUNCTION__ << endl;
+    for(int i = 0; i < size; ++i)
+    {
+        x[i] = pMapFunc(x[i]);//change value for each element
+    }
+    return x;
+}
+//---------------------------------------------
+template<typename T>
+T Redus(T *const x, const int& size, T (*pRedFunc)(T, T))
+{
+    cout << __FUNCTION__ << endl;
+    T value = 0;
+    for(int i = 0; i < size; ++i)
+    {
+      value = pRedFunc(value, x[i]);
+    }
+    return value;
+}
+
+//---------------------------------------------
+template<typename T>
+T *Filter(T x[], const int& size, int& newSize, bool (*Filt_ptr_f)(T))
+{
+    cout << __FUNCTION__ << endl;
+    int j = 0;
+    for(int i = 0; i < size; i ++)
+    {
+        if(Filt_ptr_f(x[i]))
+        {//filtering mas by user function
+            x[j] = x[i];
+            j++;
+        }
+    }
+    newSize = j;//new size mas
+    return x;
+}
+//---------------------------------------------
+
+
+#endif // TEMPLATEFUNCTION_H
