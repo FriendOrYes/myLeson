71871e8d09c29601fb475148eba8783167428015
FriendOrYes <anton.chaban@mail.ru>
2017-02-15 15:54:44 +0300

    Task 20

--- Task20.cpp
+++ Task20.cpp
@@ -1,269 +1,188 @@
 #include <iostream>
-#include <cassert>
-#include <cstring>
-#include <cmath>
+#include <math.h>
+#include <stdio.h>
+#include <string>
 
 using namespace std;
 
-#define ARRAY_SIZE(a) (sizeof(a) / sizeof(a[0]))
 
-template<class T>
-class Stack
+#define M_PI 3.14
+//---------------------------------------------------------
+
+struct Shape;
+
+//---------------------------------------------------------
+
+typedef void (*init_ptr_t)(Shape*);
+typedef float (*squre_ptr_t)(Shape*);
+
+//---------------------------------------------------------
+
+struct Vtable
 {
-    size_t m_maxSize;
-    T* m_stackPtr;
-    int m_top;
-public:
-    Stack(const int& size = 10)
-        : m_maxSize(size)
-        , m_stackPtr(new T[m_maxSize])
-        , m_top(0)
-    {
+   squre_ptr_t squre;
 
-    }
-    Stack(const Stack& obj)
-        : m_maxSize(obj.m_maxSize)
-        , m_stackPtr(new T[m_maxSize])
-        , m_top(obj.m_top)
-    {
-        for(size_t i = 0; i < m_top; ++i)
-        {
-            m_stackPtr[i] = obj.m_stackPtr[i];
-        }
-    }
-    ~Stack()
-    {
-        delete []m_stackPtr;
-    }
-
-    inline void Push(const T& elem)
-    {
-        if(m_top > m_maxSize)
-        {
-            throw runtime_error("Stack is Full");
-        }
-
-        m_stackPtr[m_top++] = elem;
-    }
-    inline const T& Pop()
-    {
-        if(m_top < 1)
-        {
-            throw runtime_error("Stack is empty");
-        }
-
-        return m_stackPtr[m_top--];
-    }
-    const size_t& GetStackSize()const
-    {
-        return m_maxSize;
-    }
-
-    T *GetPtr()const
-    {
-        return m_stackPtr;
-    }
-
-    const int& GetTop()const
-    {
-        return m_top;
-    }
-
-    template<typename U>
-    friend ostream& operator << (ostream &os, const Stack<U>& obj);
-    const T& operator = (const Stack& obj) = delete;
-};
-//-------------------------------------------------------------------------
-//-------------------------------------------------------------------------
-template<>
-class Stack<bool>
-{
-
-    size_t m_maxSize;
-    char* m_stackPtr;
-    int m_top;
-
-    size_t SizeСalculation(const int& size)
-    {
-        m_maxSize = size / 8;
-        if(size%8)
-        {
-            m_maxSize++;
-        }
-
-        return m_maxSize;
-    }
-public :
-    Stack(const int& size = 15)
-        : m_stackPtr(new char[SizeСalculation(size)])
-        , m_top(0)
-    {
-    }
-
-    Stack(const Stack& obj)
-        : m_maxSize(obj.m_maxSize)
-        , m_stackPtr(new char[m_maxSize])
-        , m_top(obj.m_top)
-    {
-        cout << "Stack(copy) " << endl;
-        memcpy(m_stackPtr,obj.m_stackPtr,sizeof(*m_stackPtr) * m_maxSize);
-    }
-
-    ~Stack()
-    {
-        delete []m_stackPtr;
-    }
-//-------------------------------------------------------------------------
-    inline void Push(const bool& elem)
-    {
-        if(m_top > m_maxSize * 8 - 1)
-        {
-            throw runtime_error("Stack is overFloow");
-        }
-
-        int numElem = m_top / 8;
-        int offsetElem = m_top % 8;
-
-        m_stackPtr[numElem] ^= (-elem ^ m_stackPtr[numElem])
-                & (1 << (7 - offsetElem));
-        m_top++;
-    }
-//-------------------------------------------------------------------------
-
-    bool Pop()
-    {
-        if(m_top < 1)
-        {
-            throw runtime_error("Stack empty!");
-        }
-
-        int numElemet = m_top / 8;
-        int offsetElem = m_top % 8;
-
-        m_top--;
-
-        return (1 & (m_stackPtr[numElemet] >> offsetElem));
-    }
-//-------------------------------------------------------------------------
-    size_t GetMaxStackLenght()const
-    {
-        return m_maxSize * 8;
-    }
-
-    const int& GetTop()const
-    {
-        return m_top;
-    }
-//-------------------------------------------------------------------------
-    const Stack& operator = (const Stack& obj)
-    {
-
-        if(this != &obj)
-        {
-            Stack tmp(obj);
-            swap(tmp);
-        }
-        return *this;
-    }
-
-    void swap(Stack& obj)
-    {
-          std::swap(obj.m_maxSize, m_maxSize);
-          std::swap(obj.m_stackPtr, m_stackPtr);
-          std::swap(obj.m_top, m_top);
-    }
-    friend ostream& operator << (ostream &os, const Stack& obj);
 };
 
-
-//-------------------------------------------------------------------------
-
-template<typename T>
-ostream& operator << (ostream &os, const Stack<T>& obj)
+//---------------------------------------------------------
+struct Shape
 {
-    for(size_t i = 0; i < obj.m_top; i++)
-    {
-        os << obj.m_stackPtr[i] << endl;
-    }
-    return os;
+    Vtable *pVtable;
+    init_ptr_t shapeInit;
+    float squreShape;
+};
+//---------------------------------------------------------
+struct Triangle
+{
+    Shape pBase;
+    Vtable *pVtable;
+    float a;
+    float b;
+    float c;
+};
+//---------------------------------------------------------
+struct Circle
+{
+    Shape pBase;
+    float radius;
+    Vtable *pVtable;
+};
+//---------------------------------------------------------
+struct Rectangle
+{
+    Shape pBase;
+    float weight;
+    float height;
+    Vtable *pVtable;
+
+};
+//---------------------------------------------------------
+float TriangleSquare(Shape *thiz)
+{
+    cout << " Triangle Square " << endl;
+    float S = 0;
+    Triangle* pTrianle = (Triangle*)thiz;
+
+    float p = (pTrianle->a + pTrianle->b + pTrianle->c)/2;
+    pTrianle->pBase.squreShape = sqrt(p * (p - pTrianle->a) * (p-pTrianle->b) * (p-pTrianle->c));
+    cout <<" S = " << pTrianle->pBase.squreShape << endl ;
+    S += pTrianle->pBase.squreShape;
+
+    return S;
+}
+//---------------------------------------------------------
+float RectangleSquare(Shape *thiz)
+{
+    cout << " Rectangle Square " << endl;
+    float S = 0;
+    Rectangle* pRectangle = (Rectangle*)(thiz) ;
+
+    pRectangle->pBase.squreShape = pRectangle->height * pRectangle->weight;
+    cout <<" S = " << pRectangle->pBase.squreShape << endl ;
+    S += pRectangle->pBase.squreShape;
+
+    return S;
+}
+//---------------------------------------------------------
+float CircleSquare(Shape *thiz)
+{
+    cout << " Circl Square " << endl;
+    float S = 0;
+    Circle* pCircle = (Circle*)(thiz);
+
+    pCircle->pBase.squreShape = M_PI * pow(pCircle->radius,2);
+    cout <<" S = " << pCircle->pBase.squreShape << endl ;
+    S += pCircle->pBase.squreShape;
+    return S;
+}
+//---------------------------------------------------------
+void ShapeInit(Shape* thiz)
+{
+    /* >>> VTable construction START >>> */
+    Vtable* vptr = new Vtable;
+    thiz->pVtable = vptr;
+    thiz->pVtable->squre = NULL; // pure function
+    /* <<< VTable construction END <<< */
+    cout << "I'm pure person" << endl;
+    thiz->squreShape = 0;
+}
+//---------------------------------------------------------
+void RectangleInit(Rectangle* thiz, const float& height, const float& weight)
+{
+        ShapeInit((Shape*)thiz);
+        cout << "Rectangle Init :" << endl;
+        /* >>> VTable construction START >>> */
+        Vtable *vptr = new Vtable;
+        thiz->pVtable = vptr;
+        thiz->pBase.pVtable->squre = RectangleSquare;
+        /* <<< VTable construction END <<< */
+        thiz->height = height;
+        thiz->weight = weight;
+}
+//---------------------------------------------------------
+void CircleInit(Circle* thiz, const float& radius)
+{
+
+        ShapeInit((Shape*)thiz);
+        cout << "Circl Init :" << endl;
+
+        /* >>> VTable construction START >>> */
+
+        Vtable *vptr = new Vtable;
+        thiz->pVtable = vptr;
+        thiz->pBase.pVtable->squre = CircleSquare;
+
+        /* <<< VTable construction END <<< */
+
+        thiz->radius = radius;
+}
+//---------------------------------------------------------
+void TriangleInit(Triangle* thiz, const float& a, const float &b, const float& c)
+{
+        ShapeInit((Shape*)thiz);
+
+        cout << "Triangle Init :" << endl;
+
+        Vtable *vptr = new Vtable;
+        thiz->pVtable = vptr;
+        thiz->pBase.pVtable->squre = TriangleSquare;
+        thiz->a = a;
+        thiz->b = b;
+        thiz->c = c;
+}
+//---------------------------------------------------------
+void PrintSquereShape(Shape *sh)
+{
+    cout << sh->pVtable->squre(sh) <<endl;
 }
 
-//--------------------------------------------------------------------------
-ostream& operator << (ostream &os, const Stack<bool>& obj)
+void Clear(Shape** sh, const int& size)
 {
-    int i = 0, k = 7;
-    int offsetElem = k % 8, top = obj.m_top;
-
-    while(top--)
+    for(int i = 0; i < size; ++i)
     {
-        os << (1 & (obj.m_stackPtr[i] >> offsetElem)) << " | ";
-
-        if(k-- == 0)
-        {
-            k = 7;
-        }
-        i = (obj.m_top - top) / 8;
-        offsetElem = k % 8;
+        delete sh[i];
     }
-    os << endl;
-    return os;
 }
-
 
 int main(int argc, char *argv[])
 {
-    const int size = 10;
+    const int size = 3;
 
-    bool flag = false;
-    Stack<bool> st;
-    try
-    {
-        st.Push(1);
-        st.Push(1);
-        st.Push(1);
-        st.Push(0);
-        st.Push(1);
-        st.Push(1);
-        st.Push(0);
-        st.Push(0);
-        st.Push(1);
-        st.Push(1);
-        st.Push(1);
-        st.Push(1);
-        st.Push(0);
-        st.Push(1);
-        st.Push(1);
-        st.Push(0);
-        st.Push(0);
-        st.Push(1);
-        st.Push(1);
-        st.Push(1);
-        st.Push(1);
-        st.Push(0);
-        st.Push(1);
-        st.Push(1);
-        st.Push(0);
-        st.Push(0);
-        st.Push(1);
-    }
-    catch(exception &ex)
-    {
-        flag = true;
-    }
-    assert(flag == true);
-    Stack<bool> st2;
 
-    st2 = st;
-    cout << st;
-    Stack<double> stD(8);
+    Triangle *triangl = new Triangle();
+    Rectangle *rectangl = new Rectangle();
+    Circle *circl = new Circle();
 
-    stD.Push(5);
-    stD.Push(3.4);
-    stD.Push(2.3);
-    stD.Push(1.4);
-    stD.Push(-6.1);
+    TriangleInit(triangl, 2, 3, 4);
+    RectangleInit(rectangl, 4, 5);
+    CircleInit(circl, 10);
 
-    cout << st;
-    cout << stD;
+    Shape *sh[size] = {(Shape*)triangl,(Shape*)circl,(Shape*)rectangl};//static_cast<Shape*>
+
+    PrintSquereShape(sh[0]);
+
+    Clear(sh, size);
+
     return 0;
 }
+//---------------------------------------------------------
